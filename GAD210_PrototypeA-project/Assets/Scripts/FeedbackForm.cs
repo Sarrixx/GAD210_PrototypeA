d3328c98890a2f4d6505648f0c04d2bc81ef6e2b
Sarrixx <joshua.fizz.ferguson@gmail.com>
2024-06-09 18:13:20 +0800

    - Implemented quit functionality - Setup project build settings for first prototype - Added build folder to ignore

--- GAD210_PrototypeA-project/Assets/Scripts/FeedbackForm.cs
+++ GAD210_PrototypeA-project/Assets/Scripts/FeedbackForm.cs
@@ -4,6 +4,14 @@ public class FeedbackForm : MonoBehaviour
 {
     private readonly string url = "https://forms.gle/z7omXdrxX3xG645v6";
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) == true)
+        {
+            Application.Quit();
+        }
+    }
+
     private void OnApplicationQuit()
     {
         Application.OpenURL(url);
